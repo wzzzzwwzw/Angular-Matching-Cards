cc9f5aa91a0931efaadd98b4fbfd0e4bbe085d58
Wael Louati <waelwzwz@gmail.com>
2022-12-11 14:37:15 -0800

    import Materials + added routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { FooterComponent } from './footer/footer/footer.component';
 import { NavComponent } from './nav/nav/nav.component';
+import { AppRoutingModule } from './app-routing.module';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import {MatInputModule} from "@angular/material/input";
+import {HttpClientModule} from "@angular/common/http";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatTableModule} from "@angular/material/table";
+import {MatIconModule} from "@angular/material/icon";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatDialogModule} from "@angular/material/dialog";
 
 
 
@@ -21,6 +29,19 @@ import { NavComponent } from './nav/nav/nav.component';
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    MatDialogModule,
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    NoopAnimationsModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatIconModule
 
   ],
   providers: [],
