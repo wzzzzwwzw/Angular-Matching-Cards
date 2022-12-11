78e5f63edb87d634a4cc1c36d3ac8a28bb722853
Wael Louati <waelwzwz@gmail.com>
2022-12-11 13:46:48 -0800

    Added home nav footer components + images

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { FooterComponent } from './footer/footer/footer.component';
+import { NavComponent } from './nav/nav/nav.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavComponent,
+
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
