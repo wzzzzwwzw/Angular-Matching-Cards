4777212a8924692d8f86d19eaf265f982d4d84ba
Wael Louati <waelwzwz@gmail.com>
2022-12-11 15:45:09 -0800

    Added Login Component + Persistence, Http, Api Services + Refactoring

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import {HomeComponent} from "./home/home/home.component";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/nav/nav/nav.component.ts
+++ src/app/nav/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {PersistenceService} from "../../shared/services/persistence.service";
 
 @Component({
   selector: 'app-nav',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
+
+  isLogged: boolean = false;
+
+  constructor(private persistence: PersistenceService) {
+    this.isLogged = this.persistence.isLogged;
+    this.persistence.loggedChange.subscribe((value) => {
+      this.isLogged = value;
+    })
+  }
+
+  logout(): void {
+    this.persistence.setToken(null);
+  }
 
 }
